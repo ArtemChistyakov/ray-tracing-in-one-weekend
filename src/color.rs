93019d3ed96a9cc10bc50e9ad3e4be1287d5d579
artem.chistyakov <artem.chistyakov@nexign.com>
2022-03-11 20:40:47 +0300

    7.2.Generating Pixels with Multiple Samples.

--- src/color.rs
+++ src/color.rs
@@ -4,8 +4,25 @@ use crate::Vec3;
 
 pub type Color = Vec3;
 
-pub fn write_color<T: Write>(out: &mut T, pixel_color: &Color) {
-    writeln!(out, "{} {} {}", (255.999 * pixel_color.x) as i32,
-             (255.999 * pixel_color.y) as i32,
-             (255.999 * pixel_color.z) as i32).unwrap();
+pub fn write_color<T: Write>(out: &mut T, pixel_color: &Color, samples_per_pixel: usize) {
+    let mut r = pixel_color.x;
+    let mut g = pixel_color.y;
+    let mut b = pixel_color.z;
+
+    // Divide the color by the number of samples.
+    let scale = 1.0 / samples_per_pixel as f64;
+    r *= scale;
+    g *= scale;
+    b *= scale;
+
+// Write the translated [0,255] value of each color component.
+    writeln!(out, "{} {} {}", (256.0 * clamp(r, 0.0, 0.999)) as i32,
+             (256.0 * clamp(g, 0.0, 0.999)) as i32,
+             (256.0 * clamp(b, 0.0, 0.999)) as i32).unwrap();
+}
+
+pub fn clamp(x: f64, min: f64, max: f64) -> f64 {
+    if x < min { return min; }
+    if x > max { return max; }
+    x
 }
